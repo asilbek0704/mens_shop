db3c2f314d35dc317b19a24a6ff584eadea8bc90
Asilbek <Asilbek>
2020-02-04 17:16:57 +0300

    input-range

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 <button type="button" onclick="this.nextElementSibling.stepDown()">-</button>
 <input type="number" readonly class="raz"/>
 <button type="button" onclick="this.previousElementSibling.stepUp()">+</button>
+
